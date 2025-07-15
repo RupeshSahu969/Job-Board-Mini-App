fb4bb27d8796bf4c6b1603c2134fc43d58e4a630
Rupesh Sahu <rupeshsahu969@gmail.com>
2025-07-15 23:04:38 +0530

    add admin pages for get all submit application data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/jobs/:id" element={<JobDetail />} />
-    
+        <Route path="/admin" element={<Admin />} />
         <Route path="/applications/:id" element={<ApplyForm />} />
       </Routes>
     </>
