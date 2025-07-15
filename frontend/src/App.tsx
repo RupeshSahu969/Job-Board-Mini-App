41bfe4042409c7f98bb6643092a251abbe8870eb
Rupesh Sahu <rupeshsahu969@gmail.com>
2025-07-15 23:02:24 +0530

    add routes files and create Home pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
-import JobDetail from "./Pages/JobDetail";
-import ApplyForm from "./Pages/ApplyForm";
-import Admin from "./Pages/Admin";
+
 
 function App() {
   return (
@@ -12,9 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/jobs/:id" element={<JobDetail />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/applications/:id" element={<ApplyForm />} />
+       
       </Routes>
     </>
   );
